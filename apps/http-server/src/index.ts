7fe95e178553d9f906dae98e540923cfe13856c1
Nagmani001 <nagmanipd3@gmail.com>
2025-04-08 14:40:36 +0545

    asdf

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -1,12 +1,29 @@
+import { prismaClient } from "@repo/db/client";
 import express, { Request, Response } from "express";
-import { prismaClient } from "@repo/db/client"
 
 const app = express();
-
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("hji")
-})
+  res.send("hi there")
+});
+
+app.post("/signup", async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+  console.log(username);
+  console.log(password);
+  const response = await prismaClient.user.create({
+    data: {
+      username,
+      password,
+    }
+  });
+  res.json({
+    msg: "user created",
+    id: response.id
+  })
+});
+
 
 app.listen(3001, () => {
   console.log("server is running on port 3001");
--- apps/ws-server/src/index.ts
+++ apps/ws-server/src/index.ts
@@ -1,10 +1,16 @@
-import { prismaClient } from "@repo/db/client"
 import { WebSocket, WebSocketServer } from "ws";
+import { prismaClient } from "@repo/db/client";
 
 
 const wss = new WebSocketServer({ port: 8000 });
 
 
-wss.on("connection", (ws: WebSocket) => {
+wss.on("connection", async (ws: WebSocket) => {
   ws.send("hi");
+  await prismaClient.user.create({
+    data: {
+      username: Math.random().toString(),
+      password: Math.random().toString(),
+    }
+  })
 })
